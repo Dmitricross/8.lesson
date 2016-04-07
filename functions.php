440426ded8b4f2dee859c88ce792d18818f3b1dd
dmikab <dmikab@greeny.cs.tlu.ee>
2016-04-07 13:31:22 +0300

    Second part

--- functions.php
+++ functions.php
@@ -2,13 +2,53 @@
 
 require_once("../../config.php");
 
+//start server session to store data
+//in every file you want to access session
+//you should require functions
+session_start();
+
+function login($user, $pass){
+
+$pass = hash ("sha512", $pass);
+$mysql = new mysqli("localhost", $GLOBALS["db_username"], $GLOBALS["db_password"],  "webpr2016_dmikab");
+
+$stmt= $mysql->prepare("SELECT id FROM users WHERE username=? and password=?");
+
+echo $mysql->error;
+$stmt->bind_param("ss", $user, $pass);
+
+$stmt->bind_result($id);
+
+$stmt->execute();
+//get data
+
+if($stmt->fetch()){
+
+	echo "user with id ".$id." logged in!";
+
+	//create session variables
+	//redirect user
+	$_SESSION["user_id"] = $id;
+	$_SESSION["username"] =$user;
+
+	header("Location: restrict.php");
+
+}else{
+    
+    //username was wrong or password was wrong or both
+	echo "wrong credentials";
+
+}
+
+}
 function signup($user, $pass){
 
 	//GLOBALS - access outside variable in function
+	$pass = hash ("sha512", $pass);
 
 $mysql = new mysqli("localhost", $GLOBALS["db_username"], $GLOBALS["db_password"],  "webpr2016_dmikab");
 
-$stmt = $mysql->prepare("INSERT INTO users (usrname, password) VALUES (?,?) ");
+$stmt = $mysql->prepare("INSERT INTO users (username, password) VALUES (?,?) ");
 
 echo $mysql->error;
 $stmt->bind_param("ss", $user, $pass);
--- login.php
+++ login.php
@@ -1,27 +1,46 @@
 <?php
 
 require_once("functions.php");
+//Restrion - not logged in
 
+if(isset($_SESSION["user_id"])){
 
+//redirect user to restricted page
+	header("Location: restrict.php");
+
+}
 //login button clicked
-if(isset($_GET["login"])) {
+if(isset($_POST["login"])) {
 
 //login
 echo "loging in...";
 
+if(!empty($_POST["username"]) && !empty($_POST["password"])){
+
+
+
+login($_POST["username"], $_POST["password"]);
+//save to database
+
+	}else{
+
+		echo "both fields are required";
+
+	}
+
 //signing button clicked
-}else if(isset($_GET["signup"])){
+}else if(isset($_POST["signup"])){
 
 	//signup
 	echo "signing up ...";
 
 
 	//the fiekd are not empty
-	if(!empty($_GET["username"]) && !empty($_GET["password"])){
+	if(!empty($_POST["username"]) && !empty($_POST["password"])){
 
 
 
-signup($_GET["username"], $_GET["password"]);
+signup($_POST["username"], $_POST["password"]);
 //save to database
 
 	}else{
@@ -36,10 +55,10 @@ signup($_GET["username"], $_GET["password"]);
 
 <h1>Log in</h1>
 
-<form>
+<form method ="POST">
 
     <input type="text" placeholder="username" name="username">
-    <input type="password" placeholder="password" name="username">
+    <input type="password" placeholder="password" name="password">
 
     <input type="submit" name="login" value="Log in">
 
@@ -47,10 +66,10 @@ signup($_GET["username"], $_GET["password"]);
 
 <h1>Sign up</h1>
 
-<form>
+<form method ="POST">
 
     <input type="text" placeholder="username" name="username">
-    <input type="password" placeholder="password" name="username">
+    <input type="password" placeholder="password" name="password">
 
     <input type="submit" name="signup" value="Sign up">
 
